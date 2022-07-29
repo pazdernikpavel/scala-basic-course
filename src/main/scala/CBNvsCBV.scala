a8c9aa236ad630b994cd4fe5e7fc6e75ece5c493
Pavel Pazderník <pazdernik.pavel@email.cz>
2022-07-29 19:42:25 +0200

    Call by name vs. Call by value

---
+++ src/main/scala/CBNvsCBV.scala
@@ -0,0 +1,35 @@
+package udemy
+
+object CBNvsCBV extends App {
+
+  def calledByValue(x: Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  def calledByName(x: => Long): Unit = {
+    println("by name: " + x)
+    println("by name: " + x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+  // by value: 723485086649700
+  // by value: 723485086649700
+  // by name: 723485119656000
+  // by name: 723485119945100
+
+/* 
+  def calledByValue(x: Long): Unit = {
+    println("by value: " + 723485086649700L)
+    println("by value: " + 723485086649700L)
+  }
+
+  def calledByName(x: => Long): Unit = {
+    println("by name: " + System.nanoTime())
+    println("by name: " + System.nanoTime())
+  }
+ */
+
+}
